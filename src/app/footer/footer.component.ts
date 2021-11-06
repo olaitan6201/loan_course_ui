28e62d40b2efae07a1b2b7b85f1001ecb5b6c295
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2021-11-06 02:24:44 +0100

    Created App Layout

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -10,6 +10,14 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.loadJsFile("assets/js/main.js");
+  }
+
+  public loadJsFile(url: string) {
+    let node = document.createElement('script');
+    node.src = url;
+    node.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(node);
   }
 
 }
